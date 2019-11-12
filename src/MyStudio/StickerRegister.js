d19c204e3cc0c072817ffbcdd24d13709acc1904
olramde <rladhkstn8@gmail.com>
2019-11-13 01:55:45 +0900

    subpage align update

--- src/MyStudio/StickerRegister.js
+++ src/MyStudio/StickerRegister.js
@@ -7,7 +7,17 @@ class StickerRegisterPart1 extends React.Component {
   constructor() {
     super();
     this.state = {
-      stickerGenreCheckBoxData: ["Sticker", "Animated Sticker"]
+      stickerGenreCheckBoxData: ["Sticker", "Animated Sticker"],
+      supportedLanguage: [
+        "Only Image",
+        "한국어",
+        "영어",
+        "일본어",
+        "중국어",
+        "스페인어",
+        "베트남어",
+        "기타"
+      ]
     };
   }
   render() {
@@ -23,6 +33,31 @@ class StickerRegisterPart1 extends React.Component {
           isRequire={true}
           rows={2}
         ></TextAreaContent>
+        <TextAreaContent
+          title="키워드"
+          isRequire={false}
+          rows={8}
+        ></TextAreaContent>
+        <TextAreaContent
+          title="스티커 스토리"
+          isRequire={false}
+          rows={8}
+        ></TextAreaContent>
+        <CheckBoxContent
+          title="스티커 종류"
+          isRequire={true}
+          checkBox={this.state.supportedLanguage}
+        ></CheckBoxContent>
+        <CheckBoxContent
+          title="판매가격"
+          isRequire={true}
+          checkBox={["무료", "0.9달러", "1.9달러"]}
+        ></CheckBoxContent>
+        <CheckBoxContent
+          title="약관 확인"
+          isRequire={true}
+          checkBox={["약관에 동의합니다."]}
+        ></CheckBoxContent>
       </div>
     );
   }
--- src/SubPageTemplate.js
+++ src/SubPageTemplate.js
@@ -5,12 +5,12 @@ import Navigation from "./SubPage/Navigation";
 import Contents from "./SubPage/Contents";
 
 function SubPageTemplate({ header, data, contentsJSX }) {
-    return (
-        <div className="sub-page">
-            <Navigation header={header} data={data} />
-            <Contents children={contentsJSX} />
-        </div>
-    );
+  return (
+    <div className="sub-page">
+      <Navigation header={header} data={data} />
+      <Contents children={contentsJSX} />
+    </div>
+  );
 }
 
 export default SubPageTemplate;
