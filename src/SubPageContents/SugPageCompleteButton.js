15f3da4c3d03c47e2ccb7939078f03d59c5292f9
olramde <rladhkstn8@gmail.com>
2019-11-14 03:54:42 +0900

    contents align

---
+++ src/SubPageContents/SugPageCompleteButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./scss/SugPageCompleteButton.scss";
+
+function SubPageCompleteButton({ text }) {
+  return <button className="sub-page-complete-button">{text}</button>;
+}
+
+export default SugPageCompleteButton;
