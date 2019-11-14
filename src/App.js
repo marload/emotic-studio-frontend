34d52c58508e4b37ae76f2de29a972abc2122321
olramde <rladhkstn8@gmail.com>
2019-11-14 18:32:51 +0900

    footer-top complete

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route } from "react-router-dom";
 
 import Header from "./Interface/Header";
-// import Footer from "./Interface/Footer";
+import Footer from "./Interface/Footer";
 
 import Home from "./Home";
 import GuideLine from "./GuideLine";
@@ -12,48 +12,49 @@ import MyStudio from "./MyStudio";
 import "./scss/App.scss";
 
 const navigationData = [
-  {
-    title: "서비스 설정",
-    link: "/faq/service"
-  },
-  {
-    title: "계정 등록",
-    link: "/faq/account-register"
-  },
-  {
-    title: "스티커 등록",
-    link: "/faq/sticker-register"
-  },
-  {
-    title: "스티커 심사",
-    link: "/faq/sticker-judge"
-  },
-  {
-    title: "스티커 상품화",
-    link: "/faq/stciker-sell"
-  },
-  {
-    title: "수익",
-    link: "/faq/benefit"
-  },
-  {
-    title: "제휴 서비스 제도",
-    link: "/faq/alliance"
-  }
+    {
+        title: "서비스 설정",
+        link: "/faq/service"
+    },
+    {
+        title: "계정 등록",
+        link: "/faq/account-register"
+    },
+    {
+        title: "스티커 등록",
+        link: "/faq/sticker-register"
+    },
+    {
+        title: "스티커 심사",
+        link: "/faq/sticker-judge"
+    },
+    {
+        title: "스티커 상품화",
+        link: "/faq/stciker-sell"
+    },
+    {
+        title: "수익",
+        link: "/faq/benefit"
+    },
+    {
+        title: "제휴 서비스 제도",
+        link: "/faq/alliance"
+    }
 ];
 
 const App = () => {
-  return (
-    <div className="App">
-      <Header />
-      <div className="main-contents">
-        <Route exact path="/" component={Home} />
-        <Route exact path="/guide" component={GuideLine} />
-        <Route path="/faq" component={Faq}></Route>
-        <Route path="/mystudio" component={MyStudio} />
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <div className="main-contents">
+                <Route exact path="/" component={Home} />
+                <Route exact path="/guide" component={GuideLine} />
+                <Route path="/faq" component={Faq}></Route>
+                <Route path="/mystudio" component={MyStudio} />
+            </div>
+            <Footer />
+        </div>
+    );
 };
 
 export default App;
--- src/Interface/Footer.js
+++ src/Interface/Footer.js
@@ -4,4 +4,72 @@ import "./scss/Footer.scss";
 
 import { Link } from "react-router-dom";
 
+class Footer extends React.Component {
+    render() {
+        return (
+            <footer className="footer">
+                <div className="footer__top">
+                    <div className="footer__top__left footer__top--font">
+                        <div className="footer__top__content">
+                            <div className="footer__top__text-content">
+                                <div className="footer__top__title">
+                                    EmoticBox
+                                </div>
+                                <div className="footer__top__question">
+                                    다양한 이모티콘을 구매하고 싶으시다면?
+                                </div>
+                                <div className="footer__top__link">
+                                    바로가기 >
+                                </div>
+                            </div>
+                            <img
+                                className="footer__top__icon"
+                                src="images/footer-left-icon.svg"
+                                alt="emoticbox"
+                            ></img>
+                        </div>
+                    </div>
+                    <div className="footer__top__right footer__top--font">
+                        <div className="footer__top__content">
+                            <div className="footer__top__text-content">
+                                <div className="footer__top__title">
+                                    EmoticBox devloper
+                                </div>
+                                <div className="footer__top__question">
+                                    자신의 플랫폼에 EmoticBox SDK를 적용하고
+                                    싶다면?
+                                </div>
+                                <div className="footer__top__link">
+                                    바로가기 >
+                                </div>
+                            </div>
+                            <img
+                                className="footer__top__icon"
+                                src="images/footer-right-icon.svg"
+                                alt="emoticbox"
+                            ></img>
+                        </div>
+                    </div>
+                </div>
+                <div className="footer__bottom">
+                    <div className="footer__bottom__left">
+                        <div className="footer__bottom__first">
+                            <span>개인 정보 보호법</span>
+                            <span>이용약관</span>
+                        </div>
+                        <div className="footer__bottom__second">
+                            <span>Emoticbox@gmail.com</span>
+                        </div>
+                    </div>
+                    <div className="footer__bottom__right">
+                        <i class="fab fa-instagram"></i>
+                        <i class="fab fa-twitter"></i>
+                        <i class="fab fa-facebook-f"></i>
+                    </div>
+                </div>
+            </footer>
+        );
+    }
+}
+
 export default Footer;
